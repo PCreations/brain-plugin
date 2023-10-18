70a7504eec9319fb55fa5f0d3d96dbb4864854f6
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2023-10-18 23:37:26 +0200

    chore: add possibility to disable agentlabs connect

--- src/agentlabs/agentlabs.project.ts
+++ src/agentlabs/agentlabs.project.ts
@@ -34,35 +34,33 @@ export class AgentLabsProject implements OnModuleInit, OnApplicationShutdown {
   private readonly agent: ReturnType<Project['agent']>;
   private readonly serverUrl: string;
   constructor(configService: ConfigService) {
-    this.project = new Project({
-      projectId: '4f2bdf9d-0d14-4d2b-9397-074e0efdd776',
-      secret: configService.get('AGENT_LABS_SECRET_KEY'),
-      url: 'https://craft-academy.app.agentlabs.dev',
-    });
-    this.agent = this.project.agent('fcd972af-ddfe-4b1b-ba86-9f074a3629be');
-    this.serverUrl = configService.get('SERVER_URL');
+    if (configService.get('DISABLE_AGENTLABS') === undefined) {
+      this.project = new Project({
+        projectId: '4f2bdf9d-0d14-4d2b-9397-074e0efdd776',
+        secret: configService.get('AGENT_LABS_SECRET_KEY'),
+        url: 'https://craft-academy.app.agentlabs.dev',
+      });
+      this.agent = this.project.agent('fcd972af-ddfe-4b1b-ba86-9f074a3629be');
+      this.serverUrl = configService.get('SERVER_URL');
+    }
   }
 
   async onApplicationShutdown() {
-    await this.project.disconnect();
+    await this.project?.disconnect();
   }
 
   async onModuleInit() {
-    this.project.onChatMessage(async (message) => {
+    this.project?.onChatMessage(async (message) => {
       const result = await this.runPlugin(message.text);
       this.agent.send({
         text: JSON.stringify(result, null, 2),
         conversationId: message.conversationId,
       });
     });
-    await exponentialBackoff(() => this.project.connect());
+    await exponentialBackoff(() => this.project?.connect());
   }
 
   private async runPlugin(prompt: string) {
-    console.log(
-      'ai-plugin.json',
-      `${this.serverUrl}/.well-known/ai-plugin.json`,
-    );
     const tools = [
       new RequestsGetTool({
         Authorization: 'Bearer bob',
